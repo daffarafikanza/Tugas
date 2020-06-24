b16e8e06d35d228feaeaf5bbeaf729fc7122df2a
daffarafikanza <61929389+daffarafikanza@users.noreply.github.com>
2020-06-24 22:54:55 +0700

    Add files via upload

---
+++ Tugas 8/Inheritance,Polymorphsim,Abstraction,Collection/ClassAnak/Sales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Inheritance_Polymorphsim_Abstraction_Collection.ClassInduk;
+
+namespace Inheritance_Polymorphsim_Abstraction_Collection.ClassAnak
+{
+    class Sales : Karyawan
+    {
+        public double JumlahPenjualan { get; set; }
+        public double Komisi { get; set; }
+        public override double Gaji()
+        {
+            return JumlahPenjualan * Komisi;
+        }
+    }
+}
